bb92557db78220e52ce2d40490dc062c6fff9124
unknown <supasiri.benj@mail.kmutt.ac.th>
2023-05-15 16:37:52 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class program{
+    static void Main(string[] args){
+        Console.Write("B1: ");
+        float B1 = float.Parse(Console.ReadLine());
+        Console.Write("B2: ");
+        float B2 = float.Parse(Console.ReadLine());
+        Console.Write("B3: ");
+        float B3 = float.Parse(Console.ReadLine());
+
+        Console.ReadLine();
+        float currency = float.Parse(Console.ReadLine());
+
+    }
+}
